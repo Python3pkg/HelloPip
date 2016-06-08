8646d41a15b4655eeb9a47881d413c5e186e2796
Goran Iliev <goraniliev93@yahoo.com>
2016-06-08 12:30:28 +0200

    config files added

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
   author = 'Goran Iliev',
   author_email = 'goraniliev93@gmail.com',
   url = 'https://github.com/goraniliev/HelloPip', # use the URL to the github repo
-  download_url = 'https://github.com/goraniliev/HelloPip/tarball/0.2', # I'll explain this in a second
+  download_url = 'https://github.com/goraniliev/HelloPip/tarball/0.3', # I'll explain this in a second
   keywords = ['testing', 'logging', 'example'], # arbitrary keywords
   classifiers = [],
 )
